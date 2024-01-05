b81172b57eb24d8a49a1e9ed07eb1155cd284d6e
Anselme Massiot <mass.anselme@gmail.com>
2024-01-05 10:46:56 +0100

    feat : update the sidebar now we can see the song upload by the user

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,11 +3,19 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai"
 
+import { Song } from "@/types";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps{
+     songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+     songs
+}) => {
      const authModal = useAuthModal();
      const uploadModal = useUploadModal();
      const { user } = useUser();
@@ -28,7 +36,15 @@ const Library = () => {
                     </div>
                     <AiOutlinePlus onClick={onCLick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
                </div>
-               <div className="flex flex-col gap-y-2 mt-4 px-3">List of song</div>
+               <div className="flex flex-col gap-y-2 mt-4 px-3">
+                    {songs.map((item) =>(
+                         <MediaItem
+                              onClick={() => {}}
+                              key={item.id}
+                              data={item}
+                         />
+                    ))}
+               </div>
           </div>
      );
 }
